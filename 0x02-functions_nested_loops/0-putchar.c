4763d41467ec0ab911eb756e1c6fc067bba97d74
Sachk14 <skaisry14@gmail.com>
2022-06-07 13:46:07 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,20 @@
 #include "main.h"
 
 /**
- * main - check the code for ALX School students.
+ * main - a program that prints putchar and returns 0
  *
- * Program that prints Holberton, followed by a new line
- *
- * Return: always 0 (success)
+ * Return: always 0
  */
 
 int main(void)
 {
-	char s[] = "_putchar";
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-		_putchar(s[i]);
-		i++;
-	}
-	_putchar(10);
-
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
