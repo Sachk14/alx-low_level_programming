af54b7d905209c241934e721e5430d829b9262ce
Sachk14 <skaisry14@gmail.com>
2022-06-07 15:10:01 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - check if int c is lower
+ *
+ * @c: int checked
+ * Return: 1 if lower,0 if upper
+ */
+
+int _islower(int c)
+{
+	f (c  >= 97  &&  c  <=  122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
