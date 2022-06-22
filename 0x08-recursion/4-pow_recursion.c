68fab2db20f212eaf6a5a117e039b3a09b7b12a5
Sachk14 <skaisry14@gmail.com>
2022-06-22 12:27:10 -0700

    task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,26 +1,20 @@
 #include "main.h"
-#include <string.h>
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * factorial - function that returns the factorial of a given number.
- * @n: integer.
- * Return: integer of factorial.
+ * factorial - a function that finds the factorial of *n
+ *
+ * @n: positive number
+ *
+ *
+ * Return: factorial of @n
  */
-
 int factorial(int n)
 {
-	int i = 1;
-
-	if (i <= n)
-	{
-		int j = n * factorial(n - 1);
-
-		return (j);/* (j abs(j));*/
-	}
-	else if (n == 0)
-		return (1);
-	else
+	if (n < 0)		/*error -1 if number is negative*/
 		return (-1);
+	else if (n == 0)	/*return factorial of 0 = 1*/
+		return (1);
+	else			/*do factorial of n if its not less or equal to 0*/
+		return (n * factorial(n - 1));
 }
